b4f3a30d6fa66587cec45976cf9f12d9ba86073f
AayushPrakash24 <aayush.prakash@outlook.com>
2025-05-18 15:45:19 -0400

    correct inheritance structure

--- CardExceptions.py
+++ CardExceptions.py
@@ -1,17 +1,23 @@
-class RankException(Exception):
+class InvalidArgumentException(Exception):
+    def __init__(self, message=None):
+        if not message:
+            message = "Invalid argument provided."
+        super().__init__(message)
+
+class InvalidHandException(Exception):
+    def __init__(self, message=None):
+        if not message:
+            message = "Invalid hand exception."
+        super().__init__(message)
+
+class RankException(InvalidArgumentException):
     def __init__(self, message=None):
         if not message:
             message = "Invalid rank exception."
         super().__init__(message)
 
-class SuitException(Exception):
+class SuitException(InvalidArgumentException):
     def __init__(self, message=None):
         if not message:
             message = "Invalid suit exception."
-        super().__init__(message)
-
-class InvalidArgumentException(Exception):
-    def __init__(self, message=None):
-        if not message:
-            message = "Invalid argument provided."
         super().__init__(message)
